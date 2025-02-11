549c6e0009ec5216b9bd4ef94c7a889a86ce3b83
umemasultan <umemasultan11@gmail.com>
2025-02-12 01:04:53 +0500

    Add files via upload
    Project implemented  with codedreamer

---
+++ madlab.py
@@ -0,0 +1,28 @@
+
+
+# // Madlab Project //
+
+print("About Umema")
+Name=str(input("Enter Your Name"))
+DaughterOf=str(input("Enter Your Father Name"))
+Location=str(input("Enter Your Location "))
+Qualification=str(input("Enter Your Qualification"))
+Field=str(input("Enter Your Feild"))
+Expert=str(input("In which skill you are expert "))
+Currently=str(input("Enter Your currently skill"))
+Work_Experience=str(input("Enter Your Work Experience"))
+Future_Goals=str(input(" I want to become"))
+Interests=str(input("Enter Your Interests"))
+Future_Vision=str(input("Enter Your Future Vision"))
+
+Summary = (f"""Hello Guys, My Name is {Name}, Daughter of {DaughterOf}. 
+I live in {Location}. I recently passed {Qualification}. 
+My Field is {Field}. I am an expert in {Expert}. 
+Currently, I am learning Python to improve my skills. {Currently} 
+I am a professional frontend developer with expertise in React and Next.js. {Work_Experience} 
+My goal is to become a full-stack developer. {Future_Goals} 
+I love coding. {Interests} 
+I aspire to launch my own tech startup or product. {Future_Vision}""")
+
+print(Summary)
+
